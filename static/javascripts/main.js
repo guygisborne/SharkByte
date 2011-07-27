c5d4ce62dc5cdecc4ab3bdb3dba3827d618013f5
guygisborne <none@gmail.com>
2011-07-27 15:48:33 -0400

    forms are inactive when meal is expired

--- static/javascripts/main.js
+++ static/javascripts/main.js
@@ -1,5 +1,5 @@
 $('.message').slideDown(500).delay(2500).slideUp(800);
 
 $('input, textarea, select', '.expired').attr('disabled', 'disabled');
-$('.buttons button', '.expired').attr('class', 'inactive thinandtall');
+$('.buttons button', '.expired').attr('class', 'inactive thinandtall').removeAttr('type');
 
