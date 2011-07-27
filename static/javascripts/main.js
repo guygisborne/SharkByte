820a8e80d1dd27ed4172b15a0c6cdbcb7c6be8c4
guygisborne <none@gmail.com>
2011-07-27 15:31:12 -0400

    order menu styled for multiple order status scenarios

--- static/javascripts/main.js
+++ static/javascripts/main.js
@@ -1,1 +1,5 @@
 $('.message').slideDown(500).delay(2500).slideUp(800);
+
+$('input, textarea, select', '.expired').attr('disabled', 'disabled');
+$('.buttons button', '.expired').attr('class', 'inactive thinandtall');
+
