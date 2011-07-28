74ad69e5d7e244b6c2cb06e0e4cdd98363971677
guygisborne <none@gmail.com>
2011-07-28 18:06:40 -0400

    got menu list template set up

--- menu/urls.py
+++ menu/urls.py
@@ -4,4 +4,6 @@ from views import *
 
 urlpatterns = patterns('menu.views',
 	url(r'^order/$', 'order', name='order'), 
+	url(r'^menu-list/$', 'menuList', name='menu-list'), 
+	url(r'^order-list/(?P<menuID>\d+)/$', 'orderList', name='order-list'), 
 )
--- menu/views.py
+++ menu/views.py
@@ -10,3 +10,15 @@ from models import *
 def order(request):
 	return render_to_response('order.html', {}, context_instance=RequestContext(request))
 
+def menuList(request):
+	if not request.user.is_authenticated() or not request.user.is_staff:
+		return HttpResponseRedirect('/admin/') 
+
+	return render_to_response('menu_list.html', {}, context_instance=RequestContext(request))
+
+def orderList(request, menuID):
+	if not request.user.is_authenticated() or not request.user.is_staff:
+		return HttpResponseRedirect('/admin/')
+
+	return render_to_response('order_list.html', { 'menuID': menuID }, context_instance=RequestContext(request))
+
